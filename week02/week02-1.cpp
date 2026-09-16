dd05d7bd21f210244779b3385b0fcb44513df7f6
14750713-collab <14750713@me.mcu.edu.tw>
2026-09-16 11:42:13 +0800

    week02

---
+++ week02/week02-1.cpp
@@ -0,0 +1,12 @@
+///week02-1.cpp 練習 c++ string 字串
+///File-Save As 存檔成 .cpp檔
+#include <iostream>
+#include <string>
+
+int main ()
+{
+    std::string name; /// 使用標準的字串
+    std::cout << "請輸入你的名字: ";
+    std::cin >> name;
+    std::cout << name << " 你好, 你會用字串了";
+}
